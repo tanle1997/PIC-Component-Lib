a148b501268e1c15d7d3039c2559116e1da60072
tanle1997 <44522268+tanle1997@users.noreply.github.com>
2019-05-19 22:47:04 +0700

    Add files via upload

---
+++ shift_reg.c
@@ -0,0 +1,25 @@
+#include <xc.h>
+#include "shift_reg.h"
+void latch(void)
+{
+    LATCH=0;
+    __delay_us(2);
+    LATCH=1;
+    LATCH=0;
+}
+void shift_data(unsigned char n)
+{
+    unsigned char i;
+    CLK=0;
+    for(i=0;i<8;i++)
+    {
+        if((n & 0x80) == 0x80)
+            DATA=1;
+        else
+            DATA=0;
+        __delay_us(2);
+        n=n<<1;
+        CLK=1;
+        CLK=0;
+    }
+}
