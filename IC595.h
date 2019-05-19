9225efd1aed8b353b5d3921fff3d3947ea36819f
tanle1997 <44522268+tanle1997@users.noreply.github.com>
2019-05-19 22:45:46 +0700

    Add files via upload

---
+++ IC595.h
@@ -0,0 +1,10 @@
+#ifndef IC_595_H
+#define IC_595_H
+#endif
+#define DIN DATA
+#define LAT LATCH
+#define SCLK CLK
+void output(int x);
+// ham nhap du lieu noi tiep..
+void enable();
+// ham chot du lieu dau ra
