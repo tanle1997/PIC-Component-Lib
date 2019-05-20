bcbc183ea47ad7eba68baad29e31d76f5f6be73f
tanle1997 <44522268+tanle1997@users.noreply.github.com>
2019-05-20 14:47:13 +0700

    Update ic_realtime.h

--- ic_realtime.h
+++ ic_realtime.h
@@ -1,1 +1,12 @@
+// read time from ic 
+// must declare hour, minute, second
+void Read_time(unsigned char *hour, unsigned char *minute, unsigned char *second);
+void Write_time(unsigned char hour, unsigned char minute, unsigned char second);
+// must declare day, date, month, year
+void Read_date(unsigned char *day, unsigned char *date, unsigned char *month, unsigned char *year);
+void Write_date(unsigned char day, unsigned char date, unsigned char month, unsigned char year);
+// convert BCD2DEC
+unsigned char bcd2dec(unsigned char num);
+//convert DEC2BCD
+unsigned char dec2bcd(unsigned char num);
 
