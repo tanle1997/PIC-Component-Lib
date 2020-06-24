665c9d5162d454a947d8c5172c4ba0e18b8d5653
tanle1997 <44522268+tanle1997@users.noreply.github.com>
2020-06-24 23:40:00 +0700

    Update i2c_sw.c

--- i2c_sw.c
+++ i2c_sw.c
@@ -5,48 +5,49 @@
 //Intial interface
 void i2c_Init(void)
 {
-    SCL=0;
+    SCL = 0;
     SCL_TRIS = 0;
     __delay_us(2);
-    SDA=1;
+    SDA = 1;
     SDA_TRIS = 0;
     __delay_us(2);
-    SCL=1;
+    SCL = 1;
     __delay_us(2);
 }
 // Write a Byte
 void i2c_Write(unsigned char n)
 {
     unsigned char i;
-    SCL=0;
+    SCL = 0;
     __delay_us(2);
     SDA_TRIS = 0;
-    for(i=0;i<8;i++)
+    for(i = 0; i < 8; i++)
     {
-        SCL=0;
+        SCL = 0;
         __delay_us(2);
-        SDA=((n & 0x80)==0x80);
+        SDA = ((n & 0x80) == 0x80);
         __delay_us(2);
-        n=n<<1;
-        SCL=1;
+        n = n << 1;
+        SCL = 1;
         __delay_us(2);
     }
 }
 // Read a Byte
 unsigned char i2c_Read(void)
 {
-    unsigned char n = 0,i;
-    SCL=0;
+    unsigned char n = 0;
+    unsigned char i;
+    SCL = 0;
     __delay_us(2);
     SDA_TRIS = 1;
     __delay_us(2);
-    for(i=0;i<8;i++)
+    for(i = 0; i < 8; i++)
     {
-        n=n<<1;
-        SCL=1;
+        n = n << 1u;
+        SCL = 1;
         __delay_us(2);
-        n|=SDA;
-        SCL=0;
+        n |= SDA;
+        SCL = 0;
         __delay_us(2);
     }
     return n;
@@ -54,50 +55,50 @@ unsigned char i2c_Read(void)
 // Ack feedback
 void i2c_Ack(void)
 {
-    SCL=0;
+    SCL = 0;
     __delay_us(2);
-    SDA=0;
-    SDA_TRIS=0;
+    SDA = 0;
+    SDA_TRIS = 0;
     __delay_us(2);
-    SCL=1;
+    SCL = 1;
     __delay_us(2);
-    SCL=0;
+    SCL = 0;
     __delay_us(2);
-    SDA=1;
+    SDA = 1;
     __delay_us(2);
 }
 // NAck feedback
 void i2c_NAck(void)
 {
-    SCL=0;
-    __delay_us(2);
-    SDA=1;
-    SDA_TRIS=0;
-    __delay_us(2);
-    SCL=1;
+    SCL = 0;
+    __delay_us(2u);
+    SDA = 1;
+    SDA_TRIS = 0;
+    __delay_us(2u);
+    SCL = 1;
 }
 
 void i2c_Stop(void)
 {
-    SCL=0;
-    __delay_us(2);
+    SCL = 0;
+    __delay_us(2u);
     SDA_TRIS = 0;
-    SDA=0;
-    __delay_us(2);
-    SCL=1;
-    __delay_us(2);
-    SDA=1;
+    SDA = 0;
+    __delay_us(2u);
+    SCL = 1;
+    __delay_us(2u);
+    SDA = 1;
 }
 
 void i2c_Start(void)
 {
-    SCL=1;
-    __delay_us(2);
-    SDA=1;
+    SCL = 1;
+    __delay_us(2u);
+    SDA = 1;
     SDA_TRIS = 0;
-    __delay_us(2);
-    SDA=0;
-    __delay_us(2);
+    __delay_us(2u);
+    SDA = 0;
+    __delay_us(2u);
     SCL = 0;
     __delay_us(2);
 }
