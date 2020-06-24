17525479396eb67a2231dac0fdfffbc618eb9811
tanle1997 <44522268+tanle1997@users.noreply.github.com>
2020-06-24 23:22:01 +0700

    Update shift_reg.c

--- shift_reg.c
+++ shift_reg.c
@@ -1,23 +1,23 @@
 #include "shift_reg.h"
 void latch(void)
 {
-    LATCH=0;
+    LATCH = 0u;
     __delay_us(2);
-    LATCH=1;
+    LATCH = 1u;
     __delay_us(2);
-    LATCH=0;
+    LATCH = 0u;
 }
 void shift_data(unsigned char n)
 {
     unsigned char i;
-    CLK=0;
-    for(i=0;i<8;i++)
+    CLK = 0;
+    for(i = 0; i < 8; i++)
     {
-        DATA=((n & 0x80)==0x80);
-        __delay_us(2);
-        n=n<<1;
-        CLK=1;
-        __delay_us(2);
-        CLK=0;
+        DATA = ((n & 0x80) == 0x80);
+        __delay_us(2u);
+        n = n << 1u;
+        CLK = 1;
+        __delay_us(2u);
+        CLK = 0;
     }
 }
